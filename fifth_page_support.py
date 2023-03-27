d4f02d2b56a28e8fd3841341d57ba06e653fd16a
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-03-28 00:40:49 +0530

    fix import only support python 3

--- fifth_page_support.py
+++ fifth_page_support.py
@@ -1,17 +1,9 @@
 
 import sys
 
-try:
-    import Tkinter as tk
-except ImportError:
-    import tkinter as tk
-
-try:
-    import ttk
-    py3 = False
-except ImportError:
-    import tkinter.ttk as ttk
-    py3 = True
+import tkinter as tk
+import tkinter.ttk as ttk
+py3 = True
 
 def init(top, gui, *args, **kwargs):
     global w, top_level, root
