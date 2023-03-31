a7f7453066102435c508c10bff3de290ea19d0d9
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-03-31 16:02:55 +0530

    add guide

--- modules/component.py
+++ modules/component.py
@@ -1,3 +1,11 @@
+# This is the base structure of a component inside
+# the modules folder
+#
+# all the methods inside the class with run automcatically
+# to blacklist a method from running, add it to
+#                                       modules/exlcude.py
+
+
 import time
 
 
@@ -16,5 +24,5 @@ class Component:
             time.sleep(4)
 
     @classmethod
-    def func3(self):
+    def func3(self):  # this function has been exlcuded. check modules/exclude.py
         print("Running modules.component.func3()")
--- modules/exclude.py
+++ modules/exclude.py
@@ -1,6 +1,7 @@
 
-# static method names to exclude
+# function names to exclude
+# include ONLY the name of the function
 
 l: list = [
-    "func3",
+    "func3",  # this function is blacklisted
 ]
